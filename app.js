3029f4e3810cdaf5a7cd35dfd9f6dbecf679cf37
Blackstock <charlieblackstock@gmail.com>
2015-08-27 14:30:27 -0600

    working superhero db

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+// mongoose config
+require('./database');
+
+var methodOverride = require('method-override');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var api = require('./routes/api');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var swig = require('swig');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var swig = new swig.Swig();
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +30,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(methodOverride('_method'));
+
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
